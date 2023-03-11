ed1312c4941581ec14dd08072b0eeaf2f3610fca
Paguro <ivankosaev2006@gmail.com>
2023-03-11 11:28:26 +0300

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Animal Murzik = new Cat("Мурзик", "Британский вислоухий", "Ласковый");
+        Dog Scharik = new Dog("Шарик", "Дворняга", "Сторожевой");
+        Horse Angel = new Horse("Ангел", "Гнедой", 30);
+        Murzik.makeNoise();
+        Murzik.eat();
+        Murzik.sleep();
+        Scharik.makeNoise();
+        Scharik.eat();
+        Scharik.sleep();
+        Angel.makeNoise();
+        Angel.eat();
+        Angel.sleep();
+        Veterinary.treatAnimal(Murzik);
+        Veterinary.treatAnimal(Scharik);
+        Veterinary.treatAnimal(Angel);
     }
 }
